8b00c355e76d32fa49a82069a6b5b04efffd6e80
dholmes3514 <dholmes3514@gmail.com>
2025-07-24 00:13:08 +0000

    Initail run

--- src/app.py
+++ src/app.py
@@ -1,34 +1,21 @@
-import os
-import pickle
-import numpy as np
-from flask import Flask, render_template, request
+from pickle import load
+import streamlit as st
 
-app = Flask(__name__)
+model = load(open("../models/decision_tree_classifier_default_42.sav", "rb"))
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica"
+}
 
-# Load ML model from models folder
-model_path = os.path.join(os.path.dirname(__file__), '..', 'models', 'iris_model.pkl')
+st.title("Iris - Model prediction")
 
-with open(model_path, 'rb') as f:
-    model = pickle.load(f)
+val1 = st.slider("Petal width", min_value = 0.0, max_value = 4.0, step = 0.1)
+val2 = st.slider("Petal length", min_value = 0.0, max_value = 4.0, step = 0.1)
+val3 = st.slider("Sepal width", min_value = 0.0, max_value = 4.0, step = 0.1)
+val4 = st.slider("Sepal length", min_value = 0.0, max_value = 4.0, step = 0.1)
 
-species = ['Setosa', 'Versicolor', 'Virginica']
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        features = [float(request.form.get(f)) for f in ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    except (TypeError, ValueError):
-        return render_template('index.html', prediction_text="Please enter valid numbers.")
-
-    data = np.array(features).reshape(1, -1)
-    pred = model.predict(data)[0]
-    prediction_text = f"Predicted Iris species: {species[pred]}"
-    return render_template('index.html', prediction_text=prediction_text)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))   # Heroku assigns PORT; fallback to 5000 locally
-    app.run(host='0.0.0.0', port=port, debug=True)
+if st.button("Predict"):
+    prediction = str(model.predict([[val1, val2, val3, val4]])[0])
+    pred_class = class_dict[prediction]
+    st.write("Prediction:", pred_class)
